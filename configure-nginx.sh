d3f56339b8d85b0b2a0c61dbee5a865b0ed11181
Godson Ositadinma <prestigegodson@gmail.com>
2022-02-23 06:11:04 +0100

    update

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -4,9 +4,9 @@ echo "****************************************"
 echo "  ENTER NGINX SERVER BLOCK CREDENTIALS  "
 echo "****************************************"
 echo ''
-echo 'Enter domain name. eg: example.com : '
+echo 'Enter domain name. eg: artandcodes.com : '
 read DOMAIN_NAME
-echo 'Enter BASE URL. eg: /test/api/ :  '
+echo 'Enter BASE URL. eg: /sell/api/ :  '
 read BASE_URL
 echo 'Enter production server port e.g 8080: '
 read PROD_SERVER_PORT
@@ -17,43 +17,43 @@ sudo mkdir -p "/var/www/$DOMAIN_NAME/html"
 sudo chmod -R 755 "/var/www/$DOMAIN_NAME/html"
 sudo cp index.html "/var/www/$DOMAIN_NAME/html/index.html"
 
-sudo mkdir -p "/var/www/staging.$DOMAIN_NAME/html"
-sudo chmod -R 755 "/var/www/staging.$DOMAIN_NAME/html"
-sudo cp index.html "/var/www/staging.$DOMAIN_NAME/html/index.html"
+sudo mkdir -p "/var/www/sell.$DOMAIN_NAME/html"
+sudo chmod -R 755 "/var/www/sell.$DOMAIN_NAME/html"
+sudo cp index.html "/var/www/sell.$DOMAIN_NAME/html/index.html"
 
-sudo mkdir -p "/var/www/portal.$DOMAIN_NAME/html"
-sudo chmod -R 755 "/var/www/portal.$DOMAIN_NAME/html"
-sudo cp index.html "/var/www/portal.$DOMAIN_NAME/html/index.html"
+sudo mkdir -p "/var/www/sell-portal.$DOMAIN_NAME/html"
+sudo chmod -R 755 "/var/www/sell-portal.$DOMAIN_NAME/html"
+sudo cp index.html "/var/www/sell-portal.$DOMAIN_NAME/html/index.html"
 
-sudo mkdir -p "/var/www/staging-portal.$DOMAIN_NAME/html"
-sudo chmod -R 755 "/var/www/staging-portal.$DOMAIN_NAME/html"
-sudo cp index.html "/var/www/staging-portal.$DOMAIN_NAME/html/index.html"
+# sudo mkdir -p "/var/www/staging-portal.$DOMAIN_NAME/html"
+# sudo chmod -R 755 "/var/www/staging-portal.$DOMAIN_NAME/html"
+# sudo cp index.html "/var/www/staging-portal.$DOMAIN_NAME/html/index.html"
 
 #Replaces all occurance of SERVER_PORT, DOMMAIN_NAME, AND BASE_URL in the nginx config file
 sudo sed -e "s/server_port/$PROD_SERVER_PORT/g" -e "s/example.com/$DOMAIN_NAME/g" -e "s|base_url|$BASE_URL|g" nginx.conf > "/etc/nginx/sites-available/$DOMAIN_NAME"
-sudo sed -e "s/server_port/$DEV_SERVER_PORT/g" -e "s/example.com/staging.$DOMAIN_NAME/g" -e "s|base_url|$BASE_URL|g" nginx.conf > "/etc/nginx/sites-available/staging.$DOMAIN_NAME"
-sudo sed -e "s/server_port/$PROD_SERVER_PORT/g" -e "s/example.com/portal.$DOMAIN_NAME/g" -e "s|base_url|$BASE_URL|g" nginx.conf > "/etc/nginx/sites-available/portal.$DOMAIN_NAME"
-sudo sed -e "s/server_port/$DEV_SERVER_PORT/g" -e "s/example.com/staging-portal.$DOMAIN_NAME/g" -e "s|base_url|$BASE_URL|g" nginx.conf > "/etc/nginx/sites-available/staging-portal.$DOMAIN_NAME"
+sudo sed -e "s/server_port/$DEV_SERVER_PORT/g" -e "s/example.com/staging.$DOMAIN_NAME/g" -e "s|base_url|$BASE_URL|g" nginx.conf > "/etc/nginx/sites-available/sell.$DOMAIN_NAME"
+sudo sed -e "s/server_port/$PROD_SERVER_PORT/g" -e "s/example.com/portal.$DOMAIN_NAME/g" -e "s|base_url|$BASE_URL|g" nginx.conf > "/etc/nginx/sites-available/sell-portal.$DOMAIN_NAME"
+# sudo sed -e "s/server_port/$DEV_SERVER_PORT/g" -e "s/example.com/staging-portal.$DOMAIN_NAME/g" -e "s|base_url|$BASE_URL|g" nginx.conf > "/etc/nginx/sites-available/staging-portal.$DOMAIN_NAME"
 
 if [ ! -f "/etc/nginx/sites-enabled/$DOMAIN_NAME" ]
 then
     sudo ln -s "/etc/nginx/sites-available/$DOMAIN_NAME" /etc/nginx/sites-enabled/
 fi
 
-if [ ! -f "/etc/nginx/sites-enabled/staging.$DOMAIN_NAME" ]
+if [ ! -f "/etc/nginx/sites-enabled/sell.$DOMAIN_NAME" ]
 then
-    sudo ln -s "/etc/nginx/sites-available/staging.$DOMAIN_NAME" /etc/nginx/sites-enabled/
+    sudo ln -s "/etc/nginx/sites-available/sell.$DOMAIN_NAME" /etc/nginx/sites-enabled/
 fi
 
-if [ ! -f "/etc/nginx/sites-enabled/portal.$DOMAIN_NAME" ]
+if [ ! -f "/etc/nginx/sites-enabled/sell-portal.$DOMAIN_NAME" ]
 then
-    sudo ln -s "/etc/nginx/sites-available/portal.$DOMAIN_NAME" /etc/nginx/sites-enabled/
+    sudo ln -s "/etc/nginx/sites-available/sell-portal.$DOMAIN_NAME" /etc/nginx/sites-enabled/
 fi
 
-if [ ! -f "/etc/nginx/sites-enabled/staging-portal.$DOMAIN_NAME" ]
-then
-    sudo ln -s "/etc/nginx/sites-available/staging-portal.$DOMAIN_NAME" /etc/nginx/sites-enabled/
-fi
+# if [ ! -f "/etc/nginx/sites-enabled/staging-portal.$DOMAIN_NAME" ]
+# then
+#     sudo ln -s "/etc/nginx/sites-available/staging-portal.$DOMAIN_NAME" /etc/nginx/sites-enabled/
+# fi
 
 if [ -f "/etc/nginx/sites-enabled/default" ]
 then
--- install-postgres.sh
+++ install-postgres.sh
@@ -12,18 +12,18 @@ service postgresql start
 echo "**************************************"
 echo "        DATABASE CREDENTIALS          "
 echo "**************************************"
-echo "Enter Production Database Name: "
+echo "Enter Database Name: "
 read PROD_DB_NAME
-echo "Enter Development Database Name: "
-read DEV_DB_NAME
+# echo "Enter Development Database Name: "
+# read DEV_DB_NAME
 echo "Enter DB username: "
 read DB_USER_NAME
 echo "Enter DB password: "
 read DB_PASSWORD
 
 sudo -u postgres psql -d postgres -c "CREATE DATABASE $PROD_DB_NAME"
-sudo -u postgres psql -d postgres -c "CREATE DATABASE $DEV_DB_NAME" 
+# sudo -u postgres psql -d postgres -c "CREATE DATABASE $DEV_DB_NAME" 
 sudo -u postgres psql -d postgres -c "CREATE ROLE $DB_USER_NAME LOGIN" 
 sudo -u postgres psql -d postgres -c "ALTER ROLE $DB_USER_NAME WITH PASSWORD '$DB_PASSWORD'"
 sudo -u postgres psql -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE $PROD_DB_NAME to $DB_USER_NAME"
-sudo -u postgres psql -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE $DEV_DB_NAME to $DB_USER_NAME"
+# sudo -u postgres psql -d postgres -c "GRANT ALL PRIVILEGES ON DATABASE $DEV_DB_NAME to $DB_USER_NAME"
